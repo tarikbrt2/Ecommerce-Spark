0903d86b4e231cbb83e391c3431e7b1c8bf8fa2e
Tarik <tarik.brt2@gmail.com>
2020-12-14 19:03:49 +0100

    1.3 - Updated file structure and fixed minor bugs

--- models/order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-    createdAt: Date,
-    cost: Number,
-    deliveryAddress: String
-  });
-
-module.exports = Order = mongoose.model("Order", orderSchema);
--- models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-    name: String,
-    quantity: Number,
-    vendor: String
-  });
-
-module.exports = Product = mongoose.model("Product", productSchema);
