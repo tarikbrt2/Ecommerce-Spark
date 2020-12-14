5e9305cc390b00a2b1c06e545457f6e23847d1dd
Tarik <tarik.brt2@gmail.com>
2020-12-14 17:54:38 +0100

    1.2 - Customers routing done ( beta phase )

--- routes/customer.js
+++ routes/customer.js
@@ -14,13 +14,13 @@ const { customerValidation } = require('../validation/validation');
  */
 router.post('/', async (req, res) => {
     const { error } = customerValidation(req.body);
-    if(error){
+    if (error) {
         res.status(404).json({ error: error.details[0].message });
     }
     else {
         let customer = new Customer(req.body);
         customer = await customer.save();
-        res.status(201).json({ msg: 'Customer successfully registered.'});
+        res.status(201).json({ customer, msg: 'Customer successfully registered.' });
     }
 })
 
@@ -31,11 +31,49 @@ router.post('/', async (req, res) => {
  */
 router.get('/:id', (req, res) => {
     Customer.findById(req.params.id, (err, customer) => {
-        if(err){
-            res.status(404).json({ error: 'User not found.'});
+        if (err) {
+            res.status(404).json({ error: 'Customer not found.' });
         }
-        else{
+        else {
             res.status(200).json(customer);
+        }
+    })
+})
+
+/**
+ * @route PUT /api/customers/:id
+ * @desc Showing customer info
+ * @access Private
+ */
+router.put('/:id', (req, res) => {
+    const { error } = customerValidation(req.body);
+    if (error) {
+        res.status(404).json({ error: error.details[0] });
+    }
+    else {
+        Customer.findByIdAndUpdate(req.params.id, req.body, (err, customer) => {
+            if (err) {
+                res.status(404).json({ error: 'Customer not found.' });
+            }
+            else {
+                res.status(200).json({ customer, msg: 'Customer successfully updated.' });
+            }
+        })
+    }
+})
+
+/**
+ * @route DELETE /api/customers/:id
+ * @desc Deleting customer from Database
+ * @access Private
+ */
+router.delete('/:id', (req, res) => {
+    Customer.findByIdAndDelete(req.params.id, (err, customer) => {
+        if (err) {
+            res.status(404).json({ error: 'User not found.' });
+        }
+        else {
+            res.status(200).json({ customer, msg: 'User successfully deleted.' });
         }
     })
 })
